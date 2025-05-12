616c6750c5b6d74606d67372fae23188b758153a
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-05-12 15:31:02 +0100

    feat:added transaction saving

--- src/main/java/com/zele/entities/Transaction.java
+++ src/main/java/com/zele/entities/Transaction.java
@@ -1,5 +1,46 @@
 package com.zele.entities;
 
-public class Transaction {
+import java.time.LocalDateTime;
 
+public class Transaction {
+    private LocalDateTime timestamp;
+    private Account fromAccount;
+    private Account toAccount;
+    private Double amount;
+
+
+    public Transaction( Account fromAccount, Account toAccount, Double amount) {
+        this.timestamp = LocalDateTime.now();
+        this.fromAccount = fromAccount;
+        this.toAccount = toAccount;
+        this.amount = amount;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public Account getFromAccount() {
+        return fromAccount;
+    }
+
+    public void setFromAccount(Account fromAccount) {
+        this.fromAccount = fromAccount;
+    }
+
+    public Account getToAccount() {
+        return toAccount;
+    }
+
+    public void setToAccount(Account toAccount) {
+        this.toAccount = toAccount;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
 }
