22048c771c5ddd93b2161400f1f774b455039639
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-05-12 15:31:53 +0100

    feat:gave user ability to create account

--- src/main/java/com/zele/entities/Customer.java
+++ src/main/java/com/zele/entities/Customer.java
@@ -1,6 +1,15 @@
 package com.zele.entities;
 
-public class Customer {
-   
+public class Customer extends User {
+    Account account;
+
+    public Customer(String name) {
+        super(name);
+    }
+
+    public Account createAccount(String accountType) {
+       account = new Account(accountType);
+       return account;
+    }
 
 }
